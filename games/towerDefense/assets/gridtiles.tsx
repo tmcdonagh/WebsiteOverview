53b93bdb5ff71020ba30d48ffdbc90e467c98597
tom <tmcdonagh12@gmail.com>
2018-07-08 17:57:23 -0500

    editted tsx to fit new path

--- games/towerDefense/assets/gridtiles.tsx
+++ games/towerDefense/assets/gridtiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="gridtiles" tilewidth="32" tileheight="32" tilecount="140" columns="14">
- <image source="pathfinding_tutorial/assets/gridtiles.png" width="448" height="320"/>
+ <image source="gridtiles.png" width="448" height="320"/>
  <tile id="0">
   <properties>
    <property name="collide" type="bool" value="true"/>
