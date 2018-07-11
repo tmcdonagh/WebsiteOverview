3dac4276162a1f268e31b6fafa786b41dadc5214
tom <tmcdonagh12@gmail.com>
2018-07-10 21:03:00 -0500

    tried changing game to this

--- games/towerDefense/js/load.js
+++ games/towerDefense/js/load.js
@@ -10,7 +10,8 @@ var loadState = {
     game.load.image('mainEnemy', 'img/mainEnemy.png');
 
     game.load.image('tileset', 'assets/gridtiles.png');
-    game.load.tilemapTiledJSON('map', 'assets/map.json');
+    //game.load.tilemapTiledJSON('map', 'assets/map.json');
+    this.load.tilemapTiledJSON('map', 'assets/map.json');
     
 
   },
