140ecfe1e5a79fa0c239e19b82d35712d37a4609
tom <tmcdonagh12@gmail.com>
2018-09-27 16:10:21 -0500

    Added barebones student login

---
+++ studentLogin/index.js
@@ -0,0 +1,34 @@
+var idNums = [];
+var table = document.createElement('table');
+function login(id) {
+
+  if(id % 1 == 0){
+    idNums.push(id);
+  }
+
+  updateTable();
+
+  // Clears Text Box
+  document.getElementById("loginBox").value = "";
+
+}
+function logout(id) {
+  var index = idNums.indexOf(id);
+  if (index > -1) {
+    idNums.splice(index, 1);
+    updateTable();
+    // Clears Text Box
+    document.getElementById("loginBox").value = "";
+  }
+
+}
+function updateTable() {
+  var list = "";
+  for (var i = 0; i < idNums.length; i++) {
+    list += "" + idNums[i] + "<br>";
+  }
+
+  // Sets list to list variable
+  document.getElementById("list").innerHTML = list;
+}
+
