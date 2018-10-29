73f24dbbb2450c1aec2f9587afa0bf0daeaa6282
tom <tmcdonagh12@gmail.com>
2018-10-29 12:50:50 -0500

    Changed port

--- connection/index.php
+++ connection/index.php
@@ -31,7 +31,7 @@ $password = "test";
 $dbname = "clouddb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 2162);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
