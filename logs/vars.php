660da46b91058fba2f73bc56f6c98b8667e86c38
tmcdonagh <tmcdonagh12@gmail.com>
2019-09-05 14:50:58 -0500

    Fixed ip and port numbers

--- logs/vars.php
+++ logs/vars.php
@@ -1,6 +1,6 @@
 <?php
 function getVars(){
-  $servername = "10.0.0.84"; //server ip
+  $servername = "172.18.0.3"; // Docker network ip
   $username = "test";
   $password = "test";
   $dbname = "clouddb";
@@ -8,7 +8,7 @@ function getVars(){
   $usedMem = array();
   $cpuPerc = array();
 
-  $conn = new mysqli($servername, $username, $password, $dbname, 2162);
+  $conn = new mysqli($servername, $username, $password, $dbname, 3306);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
