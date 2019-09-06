9d094b3caccf0397f3a85241a163fb9fad38f64b
tmcdonagh <tmcdonagh12@gmail.com>
2019-09-06 08:21:21 -0500

    Made ip grab from central connection.php file rather than individually changing in each file. I might move it to vars.php to make more sense

--- logs/connection.php
+++ logs/connection.php
@@ -1,33 +1,38 @@
 <?php
 function getDBData(){
-  $servername = "172.18.0.3";
-  $username = "test";
-  $password = "test";
-  $dbname = "clouddb";
+	
+	$servername = "172.18.0.3";
+	$username = "test";
+	$password = "test";
+	$dbname = "clouddb";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname, 3306);
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+	session_start();
+	$_SESSION['servername'] = $servername; // Lets other files use ip variable (see vars.php)
+	
 
-  $sql = "SELECT * FROM logs ORDER BY time DESC";
-  $result = $conn->query($sql);
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 
-  echo "<h3>Connection Loss Data</h2>";
+	$sql = "SELECT * FROM logs ORDER BY time DESC";
+	$result = $conn->query($sql);
 
-  if ($result->num_rows > 0) {
-    echo "<table><tr><th>Status</th><th>Date</th></tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "<tr><td>".$row["status"]."</td><td>".$row["time"]."</td></tr>";
-}
-echo "</table>";
-} else {
-  echo "No connection loss data";
-}
-$conn->close();
+	echo "<h3>Connection Loss Data</h2>";
+
+	if ($result->num_rows > 0) {
+		echo "<table><tr><th>Status</th><th>Date</th></tr>";
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<tr><td>".$row["status"]."</td><td>".$row["time"]."</td></tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "No connection loss data";
+	}
+	$conn->close();
 }
 getDBData();
 
--- logs/vars.php
+++ logs/vars.php
@@ -1,63 +1,66 @@
 <?php
 function getVars(){
-  $servername = "172.18.0.3"; // Docker network ip
-  $username = "test";
-  $password = "test";
-  $dbname = "clouddb";
-  $freeMem = array();
-  $usedMem = array();
-  $cpuPerc = array();
 
-  $conn = new mysqli($servername, $username, $password, $dbname, 3306);
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  $memSql = "SELECT * FROM mem ORDER BY time ASC LIMIT 50;";
-  $memResult = $conn->query($memSql);
+	session_start();
+	$servername = $_SESSION['servername']; // Gets ip from connection.php file
+	$username = "test";
+	$password = "test";
+	$dbname = "clouddb";
 
-  if ($memResult->num_rows > 0){
-    while($row = $memResult->fetch_assoc()){
-      $free = $row["free"];
-      $total = $row["total"];
-      $total = preg_replace("/[^0-9]/", "", $total); // Removes KB ending
-      $available = $row["available"];
-      $available = preg_replace("/[^0-9]/", "", $available); // Removes KB ending
-      $used = $total - $available;
-      $used = floor($used / 1000);
-      array_push($usedMem, $used);
-      
-      $free = preg_replace("/[^0-9]/", "", $free); // Removes KB ending
-      $free = floor($free / 1000); // Converts KB to MB
-      array_push($freeMem, $free); 
-      $total = floor($total / 1000);
-    }
-  }
-  else{
-  }
+	$freeMem = array();
+	$usedMem = array();
+	$cpuPerc = array();
 
-  $cpuSql = "SELECT * FROM cpu ORDER BY time ASC LIMIT 100;";
-  $cpuResult = $conn->query($cpuSql);
+	$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$memSql = "SELECT * FROM mem ORDER BY time ASC LIMIT 50;";
+	$memResult = $conn->query($memSql);
 
-  if($cpuResult->num_rows > 0){
-    while($row = $cpuResult->fetch_assoc()){
-      $perc = $row["perc"];
+	if ($memResult->num_rows > 0){
+		while($row = $memResult->fetch_assoc()){
+			$free = $row["free"];
+			$total = $row["total"];
+			$total = preg_replace("/[^0-9]/", "", $total); // Removes KB ending
+			$available = $row["available"];
+			$available = preg_replace("/[^0-9]/", "", $available); // Removes KB ending
+			$used = $total - $available;
+			$used = floor($used / 1000);
+			array_push($usedMem, $used);
 
-      array_push($cpuPerc, $perc);
-    }
-  }
+			$free = preg_replace("/[^0-9]/", "", $free); // Removes KB ending
+			$free = floor($free / 1000); // Converts KB to MB
+			array_push($freeMem, $free); 
+			$total = floor($total / 1000);
+		}
+	}
+	else{
+	}
 
-  $conn->close();
+	$cpuSql = "SELECT * FROM cpu ORDER BY time ASC LIMIT 100;";
+	$cpuResult = $conn->query($cpuSql);
 
-  $varsData = new \stdClass();
-  $varsData->free = preg_replace("/[^0-9]/", "", $free);
-  $varsData->total = preg_replace("/[^0-9]/", "", $total);
-  $varsData->freeMem = $freeMem;
-  $varsData->usedMem = $usedMem;
+	if($cpuResult->num_rows > 0){
+		while($row = $cpuResult->fetch_assoc()){
+			$perc = $row["perc"];
 
-  $varsData->cpuPerc = $cpuPerc;
+			array_push($cpuPerc, $perc);
+		}
+	}
 
-  $JSON = json_encode($varsData);
-  echo $JSON;
+	$conn->close();
+
+	$varsData = new \stdClass();
+	$varsData->free = preg_replace("/[^0-9]/", "", $free);
+	$varsData->total = preg_replace("/[^0-9]/", "", $total);
+	$varsData->freeMem = $freeMem;
+	$varsData->usedMem = $usedMem;
+
+	$varsData->cpuPerc = $cpuPerc;
+
+	$JSON = json_encode($varsData);
+	echo $JSON;
 
 }
 getVars();
