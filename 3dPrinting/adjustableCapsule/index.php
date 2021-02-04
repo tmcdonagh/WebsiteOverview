176037225389c842cb3281ef44330b0e662497e5
tmcdonagh <tmcdonagh12@gmail.com>
2021-02-03 20:51:26 -0600

    Added header and footer

---
+++ 3dPrinting/adjustableCapsule/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('/var/www/html/included/header.php') ?>
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/main.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/topDownEmpty.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/alignment.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/hooks.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/parts.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/rightSide.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/empty.jpg">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" style="filter: invert(1);" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" style="filter: invert(1);" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="col-sm">
+          <h4>Adjustable Nebula Capsule Projector Wall Mount</h1>
+          <p>By Thomas McDonagh<br>July 18, 2019</p>
+          <a href="adjustableCapsule.zip"><button type="button" class="btn btn-primary btn-lg btn-block"><img src="/assets/download-button.svg" width="18"></img> Download All Files </button></a>
+          <a href="https://github.com/tmcdonagh/3dPrinting/tree/master/projectorMount"><button type="button" style="margin-top: 2px;" class="btn btn-secondary btn-lg btn-block"><img src="/assets/githubLogo.png" width="32"></img> GitHub Link </button></a>
+
+          <a href="https://www.thingiverse.com/thing:3755169"><button type="button" style="margin-top: 2px;" class="btn btn-secondary btn-lg btn-block"><img src="/assets/thingiverseLogo.png" width="42"></img> Thingiverse Link </button></a>
+
+
+          <p>Wall mount for Nebula Capsule Projector that can change angle by turning the screw at the top. The wall hooks allow the projector to be taken off and put back up without having to unscrew anything. There is a slot at the back that still lets you plug in the micro USB and HDMI while in the mount. <a href="https://www.thingiverse.com/thing:3403165">Old Version</a></p>
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
+
+
+
+    </div>
+    <?php include('/var/www/html/included/footer.php') ?>
+
+  </body>
+</html> 
