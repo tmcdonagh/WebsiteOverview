849265f106f8e8539b1dc788af6bb29184133995
tmcdonagh <tmcdonagh12@gmail.com>
2021-02-04 09:21:12 -0600

    fixed missing include

---
+++ 3dPrinting/pspShelf/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('/var/www/html/included/header.php') ?>
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/front.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/games.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/empty.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/topDown.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/closeBase.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/leftPart.jpg">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/middlePart.jpg">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" style="filter: invert(1);" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" style="filter: invert(1);" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="col-sm">
+          <h4>Modular PSP Game Holder</h1>
+          <p>By Thomas McDonagh<br>January 16, 2018</p>
+          <a href="pspShelf.zip"><button type="button" class="btn btn-primary btn-lg btn-block"><img src="/assets/download-button.svg" width="18"></img> Download All Files </button></a>
+
+          <a href="https://www.thingiverse.com/thing:2759485"><button type="button" style="margin-top: 2px;" class="btn btn-secondary btn-lg btn-block"><img src="/assets/thingiverseLogo.png" width="42"></img> Thingiverse Link </button></a>
+
+
+          <p>PSP game holder that can be mounted on wall. Each piece connects with dovetail joints and requires no glue as long as all screw holes are used and achors are used every 3-4 games.</p>
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
+
+
+
+    </div>
+    <?php include('/var/www/html/included/footer.php') ?>
+  </body>
+</html> 
