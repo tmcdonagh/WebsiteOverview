c265f2ff7a2784aa50d1350137035f56405b054e
tmcdonagh <tmcdonagh12@gmail.com>
2021-02-14 18:34:12 -0600

    Added base folder for pcb projects

---
+++ pcbProjects/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php $currentPage = 'pcbProjects'; ?>
+    <?php include('/var/www/html/included/header.php') ?>
+    <link rel="stylesheet" type="text/css" href="/main.css">
+    <link rel="stylesheet" type="text/css" href="/menu.css">
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+        <h1>Printed Circuit Boards</h1>
+
+        <div class="album py-5 bg-light">
+          <div class="container">
+
+            <div class="row">
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <a href="/pcbProjects/distortion/"><img class="card-img-top" src="/pcbProjects/distortion/images/front.jpg" alt="front.jpg"></a>
+                  <div class="card-body">
+                    <a class="noBlue" href="/pcbProjects/distortion/"><p class="card-text">Distortion Guitar Pedal</p></a>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="/pcbProjects/distortion"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                        <a href="/pcbProjects/distortion/distortion.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+
+    <?php include('/var/www/html/included/footer.php') ?>
+
+
+
+
+
+      </center>
+  </body>
+</html> 
+
