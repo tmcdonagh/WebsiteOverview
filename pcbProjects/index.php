d31b4ea253a4c9486db639d7efca02faa84a62e3
tmcdonagh <tmcdonagh12@gmail.com>
2021-04-07 09:19:30 -0500

    Added fuzz console pedal to page

--- pcbProjects/index.php
+++ pcbProjects/index.php
@@ -10,67 +10,82 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="/bootstrap/js/bootstrap.min.js"></script>
 
-        <h1>Printed Circuit Boards</h1>
+    <h1>Printed Circuit Boards</h1>
 
-        <div class="album py-5 bg-light">
-          <div class="container">
-            <div class="row">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row">
 
-              <div class="col-md-4">
-                <div class="card mb-4 box-shadow">
-                  <a href="/pcbProjects/consolePedal/"><img class="card-img-top" src="/pcbProjects/consolePedal/images/front.JPG" alt="front.JPG"></a>
-                  <div class="card-body">
-                    <a class="noBlue" href="/pcbProjects/consolePedal/"><p class="card-text">Console Guitar Pedal</p></a>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <a href="/pcbProjects/consolePedal/"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                        <a href="/pcbProjects/consolePedal/consolePedal.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                      </div>
-                    </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="/pcbProjects/consolePedalFuzz/"><img class="card-img-top" src="/pcbProjects/consolePedalFuzz/images/front.JPG" alt="front.JPG"></a>
+              <div class="card-body">
+                <a class="noBlue" href="/pcbProjects/consolePedalFuzz/"><p class="card-text">Console Guitar Pedal Fuzz</p></a>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="/pcbProjects/consolePedalFuzz/"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                    <a href="/pcbProjects/consolePedalFuzz/consolePedalFuzz.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
                   </div>
                 </div>
               </div>
-
-              <div class="col-md-4">
-                <div class="card mb-4 box-shadow">
-                  <a href="/pcbProjects/consolePedal/"><img class="card-img-top" src="/pcbProjects/consolePedal/images/front.JPG" alt="front.JPG"></a>
-                  <div class="card-body">
-                    <a class="noBlue" href="/pcbProjects/consolePedal/"><p class="card-text">Console Guitar Pedal</p></a>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <a href="/pcbProjects/consolePedal/"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                        <a href="/pcbProjects/consolePedal/consolePedal.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 box-shadow">
-                  <a href="/pcbProjects/distortion/"><img class="card-img-top" src="/pcbProjects/distortion/images/front.PNG" alt="front.PNG"></a>
-                  <div class="card-body">
-                    <a class="noBlue" href="/pcbProjects/distortion/"><p class="card-text">Distortion Guitar Pedal</p></a>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <a href="/pcbProjects/distortion"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                        <a href="/pcbProjects/distortion/distortion.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-
             </div>
+          </div>
 
-    <?php include('/var/www/html/included/footer.php') ?>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="/pcbProjects/consolePedalDistortion/"><img class="card-img-top" src="/pcbProjects/consolePedalDistortion/images/front.JPG" alt="front.JPG"></a>
+              <div class="card-body">
+                <a class="noBlue" href="/pcbProjects/consolePedalDistortion/"><p class="card-text">Console Guitar Pedal Distortion</p></a>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="/pcbProjects/consolePedalDistortion/"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                    <a href="/pcbProjects/consolePedalDistortion/consolePedalDistortion.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="/pcbProjects/consolePedal/"><img class="card-img-top" src="/pcbProjects/consolePedal/images/front.JPG" alt="front.JPG"></a>
+              <div class="card-body">
+                <a class="noBlue" href="/pcbProjects/consolePedal/"><p class="card-text">Console Guitar Pedal</p></a>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="/pcbProjects/consolePedal/"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                    <a href="/pcbProjects/consolePedal/consolePedal.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="/pcbProjects/distortion/"><img class="card-img-top" src="/pcbProjects/distortion/images/front.PNG" alt="front.PNG"></a>
+              <div class="card-body">
+                <a class="noBlue" href="/pcbProjects/distortion/"><p class="card-text">Distortion Guitar Pedal</p></a>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="/pcbProjects/distortion"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                    <a href="/pcbProjects/distortion/distortion.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+
+        <?php include('/var/www/html/included/footer.php') ?>
 
 
 
 
 
-      </center>
+        </center>
   </body>
 </html> 
 
