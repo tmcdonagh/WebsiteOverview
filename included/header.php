aebcbccfbac0efe8ae122c6f874fa95aca6cc2b4
tmcdonagh <tmcdonagh12@gmail.com>
2021-10-25 03:14:39 +0000

    Added link to my new website in header

--- included/header.php
+++ included/header.php
@@ -33,6 +33,9 @@
         <li class="nav-item <?php if ($currentPage === 'pcbProjects') {echo 'active';} ?>">
           <a class="nav-link" href="/pcbProjects/"> PCB Projects </a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://consolepedals.com" target="_blank"> Console Pedals </a>
+        </li>
       </ul>
       <!--
         <ul class="navbar-nav ml-auto">
